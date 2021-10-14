9f3713ea97b6e4f4662de54b70239b4b88b8f87f
Tkachenko Nikita <tkachenkond00@gmail.com>
2021-10-14 14:21:32 +0300

    third commit

--- app/src/main/java/com/example/hw_3/view_model/UserViewModel.kt
+++ app/src/main/java/com/example/hw_3/view_model/UserViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.hw_3.view_model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.hw_3.model.User
@@ -9,15 +10,18 @@ class UserViewModel : ViewModel() {
 
     val userData: UserData = UserData()
 
-    var userListLiveData = MutableLiveData<UserData>()
-    var userDetailsLiveData = MutableLiveData<User>()
+    private val _userListLiveData = MutableLiveData<UserData>()
+    val userListLiveData: LiveData<UserData> = _userListLiveData
+
+    private val _userDetailsLiveData = MutableLiveData<User>()
+    val userDetailsLiveData: LiveData<User> = _userDetailsLiveData
 
     fun loadListUsers(){
-        userListLiveData.value = userData
+        _userListLiveData.value = userData
     }
 
     fun loadDetailsUser(id: Int){
-        userDetailsLiveData.value = userData.userList[id]
+        _userDetailsLiveData.value = userData.userList[id]
     }
 
 
